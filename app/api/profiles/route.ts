30442df92196e1e86ca0926d22011863b85cadb9
Patrick Melan <pjm369@drexel.edu>
2026-04-07 16:05:40 -0400

    add GET method to get user data from supabase (api/profiles/route.ts)

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -35,3 +35,30 @@ export async function POST(request: Request) {
     // return OK if successful
     return NextResponse.json({ ok: true });
 }
+
+export async function GET(request: Request) {
+
+    // assign user_id from search parameters to user_id variable
+    const user_id = new URL(request.url).searchParams.get("user_id");
+    
+    // return auth error if user_id doesn't exist
+    if (!user_id)
+        return NextResponse.json({ error: "Authentication error: user_id required" }, { status: 400 });
+
+    // get user, academics, living, and interests data from Supabase
+    // wrapped in Promise.all() for efficiency
+    const [user, academics, living, interests] = await Promise.all([
+        supabaseAdmin.from("users").select("*").eq("id", user_id).single(),
+        supabaseAdmin.from("academics").select("*").eq("user_id", user_id).single(),
+        supabaseAdmin.from("living_styles").select("*").eq("user_id", user_id).single(),
+        supabaseAdmin.from("interests").select("*").eq("user_id", user_id).single(),
+    ]);
+
+    // return all data in JSON object
+    return NextResponse.json({
+        user: user.data,
+        academics: academics.data,
+        living: living.data,
+        interests: interests.data,
+    });
+}
